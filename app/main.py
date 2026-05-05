a4b266c1d25b1a389c641e2204f1eebd73386640
Illia Savinov <dresar2404@gmail.com>
2026-05-05 15:56:57 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+import multiprocessing
 import time
 from hashlib import sha256
 
 
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORDS_TO_BRUTE_FORCE = (
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -13,20 +14,49 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
+)
 
 
-def sha256_hash_str(to_hash: str) -> str:
-    return sha256(to_hash.encode("utf-8")).hexdigest()
+def check_range(start: int, end: int, targets: set) -> list:
+    local_found = []
+    for i in range(start, end):
+        password = f"{i:08d}"
+        hashed_pass = sha256(password.encode()).hexdigest()
+
+        if hashed_pass in targets:
+            print(f"Found: {password} -> {hashed_pass}")
+            local_found.append((hashed_pass, password))
+    return local_found
 
 
-def brute_force_password() -> None:
-    pass
+def run_multiprocessing() -> dict:
+    num_processes = multiprocessing.cpu_count()
+    total_combinations = 100_000_000
+    step = total_combinations // num_processes
+
+    ranges = []
+    for i in range(num_processes):
+        start = i * step
+
+        end = (i + 1) * step if i < num_processes - 1 else total_combinations
+        ranges.append((start, end, PASSWORDS_TO_BRUTE_FORCE))
+
+    print(f"Starting {num_processes} processes...")
+
+    with multiprocessing.Pool(processes=num_processes) as pool:
+        results = pool.starmap(check_range, ranges)
+
+    final_results = {}
+    for res_list in results:
+        for hashed_pass, pwd in res_list:
+            final_results[hashed_pass] = pwd
+
+    return final_results
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    found_passwords = run_multiprocessing()
     end_time = time.perf_counter()
 
     print("Elapsed:", end_time - start_time)
